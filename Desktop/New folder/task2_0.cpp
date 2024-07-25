cdcd206368a9ac2b02fbb5bdb807e02b1d9dad43
ibraryounis <ibraryounis027@gmail.com>
2024-07-25 15:53:07 +0500

    task2

---
+++ Desktop/New folder/task2_0.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius,area ,cirumference;
+    float pi=3.14159;
+    cout<<"Enter the radius"<<endl;
+    cin>>radius;
+    area=pi*radius*radius;
+    cirumference=2*pi*radius;
+    cout<<"Area of circle is ="<<area<<endl;
+  
+    cout<<"cirumference of circle is ="<<cirumference<<endl;
+  
+}
