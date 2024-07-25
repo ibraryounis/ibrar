67e8a073deccea7898a4fc795a315c7cc51410bb
ibraryounis <ibraryounis027@gmail.com>
2024-07-25 22:10:13 +0500

    task4_1

--- Desktop/New folder/task2_0.cpp
+++ Desktop/New folder/task2_0.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
-using namespace std;
+ #include<iostream> 
+ using namespace std;
 int main()
-{
-    float radius,area ,cirumference;
-    float pi=3.14159;
-    cout<<"Enter the radius"<<endl;
+ {
+     float radius,area ,cirumference;
+     float pi=3.14159;
+     cout<<"Enter the radius"<<endl;
     cin>>radius;
     area=pi*radius*radius;
-    cirumference=2*pi*radius;
-    cout<<"Area of circle is ="<<area<<endl;
+     cirumference=2*pi*radius;
+     cout<<"Area of circle is ="<<area<<endl;
   
-    cout<<"cirumference of circle is ="<<cirumference<<endl;
+     cout<<"cirumference of circle is ="<<cirumference<<endl;
   
-}
+ }
+
---
+++ Desktop/New folder/task3_0.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number,i;
+    cout<<"enter the number=";
+    cin>>number;
+
+for(i=1;i<=10;i++)
+{
+  
+   cout<<number<<"x"<<i<<"="<<number*i<<endl;
+}
+}
