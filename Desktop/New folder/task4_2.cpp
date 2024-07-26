147e3067f8a3206362565b0c559a04283f71b40f
ibraryounis <ibraryounis027@gmail.com>
2024-07-26 07:58:17 +0500

    partice

--- Desktop/New folder/task4_2.cpp
+++ Desktop/New folder/task4_2.cpp
@@ -1,7 +1,34 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int i;
-    int num;
-}
+
+#include <iostream> 
+using namespace std; 
+
+int mostFrequent(int* arr, int n) 
+{ 
+ 
+	int maxcount = 0; 
+	int element_having_max_freq; 
+	for (int i = 0; i < n; i++) { 
+		int count = 0; 
+		for (int j = 0; j < n; j++) { 
+			if (arr[i] == arr[j]) 
+				count++; 
+		} 
+
+		if (count > maxcount) { 
+			maxcount = count; 
+			element_having_max_freq = arr[i]; 
+		} 
+	} 
+
+	return element_having_max_freq; 
+} 
+
+int main() 
+{ 
+	int arr[] = { 1,3,3,3,2,1,1,1,1,2 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	cout << mostFrequent(arr, n); 
+	return 0; 
+} 
+
+
